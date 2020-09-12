45f6f482f0bc5e0b990bea4a0fd43fa7408ee8d7
AmansGit <amans1997a@gmail.com>
2020-09-12 18:36:15 +0530

    random string

--- shortur/models.py
+++ shortur/models.py
@@ -1,9 +1,28 @@
+import random
+import string
+
 from django.db import models
 
 # Create your models here.
+
+def code_generator(size=8, chars= string.ascii_lowercase + string.ascii_uppercase + string.digits):
+	# new_code = ''
+	# for _ in range(size):
+	# 	new_code = random.choice(chars)
+	# return new_code
+	return ''.join(random.choice(chars) for _ in range(size))
+
+
 class MyUrl(models.Model):
-	url = models.CharField(max_length=300)
-	shortcode = models.CharField(max_length=18, unique=True)
+	url 		= models.CharField(max_length=300)
+	shortcode 	= models.CharField(max_length=18, unique=True)
+	update		= models.DateTimeField(auto_now=True) 		#everytime model is saved
+	timestamp	= models.DateTimeField(auto_now_add=True)	# when model was created
+	
+	def save(self, *args, **kwargs):
+		print(" hii ")
+		self.shortcode = code_generator()
+		super(MyUrl, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return str(self.url)
